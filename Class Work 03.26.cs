148c934b387e196c7677d5d3669484f33d320345
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2025-03-26 20:34:57 +0200

    Update Class Work 03.26.cs

--- Class Work 03.26.cs
+++ Class Work 03.26.cs
@@ -11,200 +11,103 @@ using System.Text.Json;
 using Game;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
+using Org.BouncyCastle.Asn1.Cms;
 
 
-class Message
-{
-    public string text { get; set; }
-    public ConsoleColor color { get; set; }
 
-    public Message(string text, ConsoleColor color) 
-    {
-        this.text = text;
-        this.color = color;
-    }
-
-}
 
 namespace Game
 {
+    
+
+    class Car
+    {
+        public int id { get; set; }
+        public int speed { get; set; }
+        public int CurentSpeed { get; set; }
+        public ConsoleColor color { get; set; }
+        public int infelicity { get; set; }
+        public double range { get; set; }
+
+        public void NewSpeed() {
+            Random random = new Random();
+            if (random.Next(1, 3) == 1)
+            {
+                this.CurentSpeed = speed + random.Next(1, infelicity);
+            }
+            else
+            {
+                this.CurentSpeed = speed - random.Next(1, infelicity);
+            }
+        }
+
+        public void Addrange()
+        {
+            this.range += ((this.CurentSpeed * 1000) / 7200);
+        }
+
+        public Car(int id, int speed, ConsoleColor color, int infelicity)
+        {
+            this.range = 0;
+            this.color = color;
+            this.infelicity = infelicity;
+            this.id = id;
+            this.speed = speed;
+            this.CurentSpeed = 0;
+        }
+
+
+    }
+
 
     class Program
     {
-        static List<Message> messages = new List<Message>();
-        public static void bubble_sort(List<int> array, int len)
+        static readonly object lockObj = new object();
+        static List<Car> cars = new List<Car>();
+        
+        static void goCar(Car car)
         {
-            int a;
-            bool is_changed = true;
-            while (is_changed)
+            while (car.range < 10000)
             {
-                is_changed = false;
-                for (int i = 0; i < len - 1; i++)
-                {
-                    if (array[i] > array[i + 1])
-                    {
-                        a = array[i];
-                        array[i] = array[i + 1];
-                        array[i + 1] = a;
-                        is_changed = true;
-                    }
-                }
-            }
-        }
-
-        public static void CountReversed(int a)
-        {
-            Thread.Sleep(200);
-            for (int i = a; i > 0; i--)
-            {
-                SendMessage(i.ToString(), ConsoleColor.Red);
                 Thread.Sleep(500);
-            }
-        }
-        public static void Count(int a)
-        {
-            for (int i = 0; i < a; i++)
-            {
-                SendMessage(i.ToString(), ConsoleColor.Green);
-                Thread.Sleep(500);
-            }
-        }
-
-        public static int Factorial(int a)
-        {
-            if (a <= 1) return 1;
-            return (a * Factorial(a - 1));
-        }
-
-        public static void QuickSortWithoutThread(List<int> list)
-        {
-            if (list.Count < 2)
-                return;
-            int pivot = list.Last();
-            List<int> list_less = new List<int>();
-            List<int> list_more = new List<int>();
-            List<int> list_equal = new List<int>();
-            for (int i = 0; i < list.Count(); i++)
-            {
-                if (pivot < list[i])
-                {
-                    list_less.Add(list[i]);
-                }
-                else if (pivot > list[i])
-                {
-                    list_more.Add(list[i]);
-                }
-                else
-                {
-                    list_equal.Add(list[i]);
-                }
+                car.Addrange();
+                car.NewSpeed();
             }
 
-            QuickSortWithoutThread(list_less);
-            QuickSortWithoutThread(list_more);
-
-            list.Clear();
-            list.AddRange(list_less);
-            list.AddRange(list_equal);
-            list.AddRange(list_more);
-        }
-
-        public static void QuickSort(List<int> list)
-        {
-            if (list.Count < 2)
-                return;
-            int pivot = list.Last();
-            List<int> list_less = new List<int>();
-            List<int> list_more = new List<int>();
-            List<int> list_equal = new List<int>();
-            for (int i = 0; i < list.Count(); i++) { 
-                if (pivot < list[i])
-                {
-                    list_less.Add(list[i]);
-                }
-                else if (pivot > list[i])
-                {
-                    list_more.Add(list[i]);
-                }
-                else
-                {
-                    list_equal.Add(list[i]);
-                }
-            }
-
-            Thread thread1 = new Thread(() => QuickSort(list_less));
-            Thread thread2 = new Thread(() => QuickSort(list_more));
-
-            thread1.Start();
-            thread2.Start();
-
-            thread1.Join();
-            thread2.Join();
-
-            
-            
-            list.Clear();
-            list.AddRange(list_less);
-            list.AddRange(list_equal);
-            list.AddRange(list_more);
-        }
-
-        static int sum1;
-        static int sum2;
-
-        public static void mySum(List<int> list)
-        {
-            List<int> list1 = new List<int>();
-            List<int> list2 = new List<int>();
-
-            for (int i = 0; i < list.Count() / 2;i++) 
-            { 
-                list1.Add(list[i]); 
-            }
-            for (int i = list.Count() / 2; i < list.Count(); i++)
-            {
-                list2.Add(list[i]);
-            }
-
-            Thread thread1 = new Thread(() => sum1 = list1.Sum());
-            Thread thread2 = new Thread(() => sum2 = list2.Sum());
-
-            thread1.Start();
-            thread2.Start();
-
-            thread1.Join();
-            thread2.Join();
-            Console.WriteLine();
-            Console.WriteLine("Sum: " + (sum1 + sum2));
-        }
-
-
-
-        static void SendMessage(string text, ConsoleColor color)
-        {
-            messages.Add(new Message(text, color));
         }
 
         public static void Main(string[] args)
         {
-            
+            Console.ResetColor();
+            Random rand = new Random();
 
-            Thread thread1 = new Thread(() => Count(5));
-            Thread thread2 = new Thread(() => CountReversed(5));
+            Console.Write("How many cars do you want? \n >");
+            var a = Convert.ToInt32(Console.ReadLine());
 
-            thread1.Start();
-            thread2.Start();
+            for (int i = 0; i < a; i++)
+            {
+                cars.Add(new Car(i + 1, rand.Next(60, 73), (ConsoleColor)rand.Next(1, 16), rand.Next(3, 8)));
+            }
+            List<Thread> list_threads = new List<Thread>();
+            foreach (var car in cars)
+            {
+                list_threads.Add(new Thread(() => goCar(car)));
+            }
+            foreach (var item in list_threads) { 
+                item.Start();
+            }
 
-
-            while (true){
-                foreach (var item in messages.ToArray())
+            Console.Clear();
+            while (true)
+            {
+                foreach (var item in cars)
                 {
                     Console.ForegroundColor = item.color;
-                    Console.WriteLine(item.text);
+                    Console.WriteLine($" {item.id, 3} | {item.CurentSpeed, 5}km/h | {Math.Round(item.range / 1000, 2), 7} / 10 km");
                     Console.ResetColor();
-                    messages.Remove(item);
                 }
-            } 
+                Console.SetCursorPosition(0, 0);
+            }
         }
     }
 }
